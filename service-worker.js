9df087beddb0befb8a591cff8cb45a15c48b97e6
rashrashrash <murashutp@gmail.com>
2019-09-03 17:05:06 +0900

    w

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ self.addEventListener('fetch', function(event) {
   //if (!event.clientId) return;
 
   // Get the client.
-  //const client = await clients.get(event.clientId);
+  const client = await clients.get(event.clientId);
   // Exit early if we don't get the client.
   // Eg, if it closed.
   //if (!client) return;
