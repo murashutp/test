7c65b16f721bece2a2a5da91dc1fa6bf41cf109e
rashrashrash <murashutp@gmail.com>
2019-09-03 17:00:49 +0900

    f

--- service-worker.js
+++ service-worker.js
@@ -8,17 +8,16 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(event) { 
-  console.log('fetch');
-  event.waitUntil(async function() {
+  // event.waitUntil(async function() {
   // Exit early if we don't have access to the client.
   // Eg, if it's cross-origin.
-  if (!event.clientId) return;
+  //if (!event.clientId) return;
 
   // Get the client.
-  const client = await clients.get(event.clientId);
+  //const client = await clients.get(event.clientId);
   // Exit early if we don't get the client.
   // Eg, if it closed.
-  if (!client) return;
+  //if (!client) return;
 
   // Send a message to the client.
   client.postMessage({
