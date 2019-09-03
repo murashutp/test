034081350ea4fa85df12ce0d823805bdd26fe099
rashrashrash <murashutp@gmail.com>
2019-09-03 16:48:26 +0900

    w

--- service-worker.js
+++ service-worker.js
@@ -7,4 +7,22 @@ self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) { 
+  event.waitUntil(async function() {
+  // Exit early if we don't have access to the client.
+  // Eg, if it's cross-origin.
+  if (!event.clientId) return;
+
+  // Get the client.
+  const client = await clients.get(event.clientId);
+  // Exit early if we don't get the client.
+  // Eg, if it closed.
+  if (!client) return;
+
+  // Send a message to the client.
+  client.postMessage({
+    msg: "Hey I just got a fetch from you!",
+    url: event.request.url
+  });
+ 
+}());});
